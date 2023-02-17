cf99ebf0e6b51c36aba15cf41304fac1494ad1e1
unknown <47340315+Tiger767@users.noreply.github.com>
2023-02-17 07:35:58 -0800

    Setup for sprint 1

---
+++ react-app/src/hooks/useBavaria.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function useBavaria() {
+    return (
+        <div>useBavaria</div>
+    )
+}
---
+++ react-app/src/hooks/useJaneHopkins.js
@@ -0,0 +1,7 @@
+import { createVendiaClient } from "@vendia/client"
+
+export default function useJaneHopkins() {
+    return (
+        <div>useJaneHopkins</div>
+    )
+}
