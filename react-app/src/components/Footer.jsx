8adb5c200aa73fc88feb966e846c41ca2a641d12
thommadison <70346704+thommadison@users.noreply.github.com>
2023-04-18 11:55:10 -0700

    Footer Updated

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer>
-      <div className="py-12 md:py-16 bg-white mt-10">
+      <div className="py-12 md:py-16 bg-white mt-10 ml-60">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           {/* Top area: Blocks */}
-          <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12">
+          <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12 ">
             {/* 1st block */}
             <div className="md:col-span-4 lg:col-span-5">
               <div className="mb-2">
